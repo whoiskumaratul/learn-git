f3dc945d0a503c0e74b97ffe3d2a56cac0770f8d
Kumar Atul Jaiswal <kumaratuljaiswal222@gmail.com>
2021-06-05 23:07:19 +0530

    added color.py

---
+++ color.py
@@ -0,0 +1,10 @@
+from colorama import Fore, Back, Style
+
+
+print("\n\n")
+
+print(Fore.RED +"http" + Fore.BLUE + "://" + Fore.GREEN +"user:password" + Fore.BLUE + "@" + Fore.LIGHTCYAN_EX+ 
+"""hackingtruth.in""" +Fore.LIGHTWHITE_EX+ ":80" + Fore.LIGHTMAGENTA_EX + "/view-article" +Fore.LIGHTYELLOW_EX+
+"""?id=10""" + Fore.RED + "#task3")    
+
+print("\n\n")
